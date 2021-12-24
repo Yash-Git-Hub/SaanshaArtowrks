07e489d5fa6c368b88f873dd5a9d6ed7c320c248
Yash-Git-Hub <69065839+Yash-Git-Hub@users.noreply.github.com>
2021-12-24 12:05:01 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd 
+import plotly.express as px
+import seaborn as sns
+description = "My first app"
+import datetime
+def run():
+
+   st.sidebar.title("Saansha Rocks")
+   url='https://raw.githubusercontent.com/Yash-Git-Hub/SaanshaArtworks/main/Data.csv'
+
+   df=pd.read_csv(url,index_col=0)
+   df['Time']= pd.to_datetime(df['Time'])
+   print(df.dtypes)
+
+   st.title ("LETS SELL SOME SUPER ULTRA MEGA PLANNERS")
+   st.write(df)
+   st.write(px.scatter(df, y="Ranking", x="Time"))
+
+
+
+
+if __name__ == "__main__":
+   run()
